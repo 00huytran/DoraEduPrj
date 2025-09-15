240e5a90c5280fd7428960facbec55fa63ba9d7b
Nart <118239463+00huytran@users.noreply.github.com>
2025-09-15 23:17:01 +0700

    update ui

--- resources/js/layouts/auth/AuthSplitLayout.vue
+++ resources/js/layouts/auth/AuthSplitLayout.vue
@@ -2,7 +2,7 @@
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
 import { home } from '@/routes';
 import { Link, usePage } from '@inertiajs/vue3';
-
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 const page = usePage();
 const name = page.props.name;
 const quote = page.props.quote;
@@ -29,13 +29,24 @@ defineProps<{
             </div>
         </div>
         <div class="lg:p-8">
-            <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                <Card class="rounded-xl">
+                    <CardHeader class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                        <CardTitle class="text-xl">{{ title }}</CardTitle>
+                        <CardDescription>
+                            {{ description }}
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent class="">
+                        <slot />
+                    </CardContent>
+                </Card>
+            <!-- <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                 <div class="flex flex-col space-y-2 text-center">
                     <h1 class="text-xl font-medium tracking-tight" v-if="title">{{ title }}</h1>
                     <p class="text-sm text-muted-foreground" v-if="description">{{ description }}</p>
                 </div>
                 <slot />
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
